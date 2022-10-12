010b519007d0e453ab6026cbd162fa6dc1e1dcbc
elitechdigital <115512919+elitechdigital@users.noreply.github.com>
2022-10-12 08:16:51 -0700

    Add files via upload

---
+++ web.js
@@ -0,0 +1,110 @@
+const quoteText = document.querySelector(".quote"),
+authorName = document.querySelector(".author .name"),
+quoteBtn = document.querySelector("button"),
+soundBtn = document.querySelector(".sound"),
+copyBtn = document.querySelector(".copy"),
+twitterBtn = document.querySelector(".twitter");
+function randomQuote(){
+    quoteBtn.classList.add("loading");
+    quoteBtn.innerText = "Loading Quote...";
+    fetch("https://api.quotable.io/random").then(res => res.json()).then(result =>{
+        quoteText.innerText = result.content;
+        authorName.innerText = result.author;
+        quoteBtn.innerText = "New Quote";
+        quoteBtn.classList.remove("loading");
+    });
+}
+soundBtn.addEventListener("click", ()=>{
+    let utterance = new SpeechSynthesisUtterance(`${quoteText.innerText} by ${authorName.innerText}`);
+    speechSynthesis.speak(utterance);
+});
+copyBtn.addEventListener("click", ()=>{
+    navigator.clipboard.writeText(quoteText.innerText);
+});
+twitterBtn.addEventListener("click", ()=>{
+    let tweetUrl = `https://twitter.com/intent/tweet?url=${quoteText.innerText}`;
+    window.open(tweetUrl, "_blank");
+});
+quoteBtn.addEventListener("click", randomQuote);
+
+let countDate = new Date('oct 19, 2022 00:00:00').getTime();
+
+function CountDown(){
+
+    let now = new Date().getTime();
+    gap = countDate - now;
+
+    let second = 1000;
+    let minute = second * 60;
+    let hour = minute * 60;
+    let day = hour * 24;
+
+    let d = Math.floor(gap / (day));
+    let h = Math.floor((gap % (day)) / (hour));
+    let m = Math.floor((gap % (hour)) / (minute));
+    let s = Math.floor((gap % (minute)) / (second));
+
+    document.getElementById('day').innerText = d;
+    document.getElementById('hour').innerText = h;
+    document.getElementById('minute').innerText = m;
+    document.getElementById('second').innerText = s;
+
+}
+
+setInterval(function(){
+    CountDown();
+},1000)
+
+function animate(obj, initVal, lastVal, duration) {
+    let startTime = null;
+
+ //get the current timestamp and assign it to the currentTime variable
+ let currentTime = Date.now();
+
+ //pass the current timestamp to the step function
+ const step = (currentTime ) => {
+
+ //if the start time is null, assign the current time to startTime
+ if (!startTime) {
+    startTime = currentTime ;
+ }
+
+ //calculate the value to be used in calculating the number to be displayed
+ const progress = Math.min((currentTime - startTime)/ duration, 1);
+
+ //calculate what to be displayed using the value gotten above
+ obj.innerHTML = Math.floor(progress * (lastVal - initVal) + initVal);
+
+ //checking to make sure the counter does not exceed the last value (lastVal)
+ if (progress < 1) {
+    window.requestAnimationFrame(step);
+ } else {
+       window.cancelAnimationFrame(window.requestAnimationFrame(step));
+    }
+ };
+ //start animating
+    window.requestAnimationFrame(step);
+ }
+ let text1 = document.getElementById('0101');
+ let text2 = document.getElementById('0102');
+ let text3 = document.getElementById('0103');
+ const load = () => {
+    animate(text1, 0, 200, 10000);
+    animate(text2, 0, 300, 10000);
+    animate(text3, 0, 100, 10000);
+ }
+
+
+ const checkbox = document.getElementById("checkbox");
+const professional = document.getElementById("professional");
+const master = document.getElementById("master");
+const basic = document.getElementById("basic");
+
+checkbox.addEventListener("click", () => {
+  basic.textContent = basic.textContent === "Ksh 7000" ? "Ksh 7500" : "Ksh 7000";
+  professional.textContent =
+    professional.textContent === "Ksh 14000" ? "Ksh 15000 " : "Ksh 14000";
+  master.textContent = master.textContent === "Ksh 23000" ? "Ksh 25000" : "Ksh 23000";
+});
+
+
